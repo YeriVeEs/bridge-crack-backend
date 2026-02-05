1aa1eaf1b2816016f578a6277605b06375bd97d1
Yeri Ventura <yeriventura@Yeris-MacBook-Air-2.local>
2026-02-04 21:07:49 -0500

    Added CORS support

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
-
     file_path = os.path.join(UPLOAD_DIR, file.filename)
 
     with open(file_path, "wb") as buffer:
